49d2549e1ba35db9c9dc8f049c412a2cdec0def8
Dany <danyweismail@gmail.com>
2017-02-17 21:01:40 +0100

    weatherApp: I have the geolocation allready

--- projects/weatherApp/weatherApp.js
+++ projects/weatherApp/weatherApp.js
@@ -1,7 +1,60 @@
-var time = "";
+
+var country_name = document.getElementById('country');
+var country_code = document.getElementById('country_code')
+var state = document.getElementById('state');
+var city = document.getElementById('city');
+var postal = document.getElementById('postal');
+var latitude = document.getElementById('latitude');
+var longitude = document.getElementById('longitude');
+var ip = document.getElementById('ipv4');
+
+
+function callback(data)
+{
+  country_code.innerHTML = data.country_code;
+  //country_name.innerHTML = data.country_name;
+  state.innerHTML = data.state;
+  city.innerHTML = data.city;
+  postal.innerHTML = data.postal;
+  latitude.innerHTML = data.latitude;
+  longitude.innerHTML = data.longitude;
+  //ip.innerHTML = data.IPv4;
+
+  
+};
+
+var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = 'https://geoip-db.com/jsonp';
+	var h = document.getElementsByTagName('script')[0];
+	h.parentNode.insertBefore(script, h);
+/*
+getJSON(api.openweathermap.org/data/2.5/weather?lat={data.latitude}&lon={data.longitude});
+*/
+
+/*
+console.log("api.openweathermap.org/data/2.5/weather?lat={data.latitude}&lon={data.longitude}");
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var time = "";
 var myTime = "";
-var location = "";
-var myLocation = "";
+var loc = document.getElementById("location");
+var myLoc = "";
 var temp = "";
 var myTemp = "";
 var logo = "";
@@ -9,46 +62,89 @@ var myLogo = "";
 var fahrenheit = "";
 
 
-// LOCATION
-var options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0
+
+
+
+var country = document.getElementById('country');
+var state = document.getElementById('state');
+var city = document.getElementById('city');
+var postal = document.getElementById('postal');
+var latitude = document.getElementById('latitude');
+var longitude = document.getElementById('longitude');
+var ip = document.getElementById('ipv4');
+
+function callback(data)
+{
+  country.innerHTML = data.country_name;
+  state.innerHTML = data.state;
+  city.innerHTML = data.city;
+  postal.innerHTML = data.postal;
+  latitude.innerHTML = data.latitude;
+  longitude.innerHTML = data.longitude;
+  ip.innerHTML = data.IPv4;
+}
+*/
+/*
+
+
+var script = document.createElement('script');
+//script.type = 'text/javascript';
+script.src = 'https://geoip-db.com/jsonp';
+var h = document.getElementsByTagName('script')[0];
+//h.parentNode.insertBefore(script, h);
+
+
+
+
+// Location avec JSON
+
+var country = document.getElementById('country');
+var state = document.getElementById('state');
+var city = document.getElementById('city');
+var postal = document.getElementById('postal');
+var latitude = document.getElementById('latitude');
+var longitude = document.getElementById('longitude');
+var country_code = document.getElementById('country_code');
+var ip = document.getElementById('ipv4');
+
+function callback (data)
+{
+  country.innerHTML = data.country_name;
+  state.innerHTML = data.state;
+  city.innerHTML = data.city;
+  postal.innerHTML = data.postal;
+  latitude.innerHTML = data.latitude;
+  longitude.innerHTML = data.longitude;
+  country_code.innerHTML = data.country_code;
+  ip.innerHTML = data.IPv4;
 };
 
-var latitude = "";
-var longitude = "";
-var accuracy = "";
+console.log(country);
 
-function success (pos) {
-  var crd = pos.coords;
-  var position = ('Your current position is :');
-  latitude = (`Latitude : ${crd.latitude}`);
-  longitude = (`Longitude : ${crd.longitude}`);
-  accuracy = (`More or less ${crd.accuracy} meters.`);
+*/
 
-  console.log(postiton);
-  console.log(latitude);
-  console.log(longitude);
-  console.log(accuracy);
-
-  //return postiton;
-  //return latitude;
-  //return  longitude;
-  //return accuracy;
+// LOCATION (code)
+/*
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    loc.innerHTML =  "Geolocation is not supported by this browser.";
+  }
 };
-//var locErr = "";
-//function error (err) {
-//  locErr =(`ERROR(${err.code}): ${err.message}`)
-//  return locErr;
-//};
 
-navigator.geolocation.getCurrentPosition(success, error, options);
+function showPosition(position) {
+  loc.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+};
+getLocation(); // get the location on load
+*/
 
+// LOCATION (town Country)
 
 
 // TEMPERATUR
-function convertTo(celsius) {
+/*function convertTo(celsius) {
   fahrenheit = celsius * 9/5 + 32;
   return fahrenheit;
 };
+*/
