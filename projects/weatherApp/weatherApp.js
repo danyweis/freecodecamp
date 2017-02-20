64ec867acb311f20d27453f50af0bf423b8c1cfe
Dany <danyweismail@gmail.com>
2017-02-20 23:28:38 +0100

    now everything is working I did have problemds with codepen, the CORS, and the http request on https now i understand why all that was like how it was

--- projects/weatherApp/weatherApp.js
+++ projects/weatherApp/weatherApp.js
@@ -1,309 +1,83 @@
-var locat = {
-lat :"",
-long :"",
-urlStart : "api.openweathermap.org/data/2.5/weather?lat=",
-key : "&appid=88ed677935f66ca8ba614806bb5b6618",
-longi : "&lon=",
-// is depend on location
-fahrenheir : "&units=imperial",
-celsius : "&units=metric"
-}
 
-urlCel = locat.urlStart + locat.lat + locat.longi + locat.long + locat.key + locat.celsius;
-urlFar = locat.urlStart + locat.lat + locat.longi + locat.long + locat.key + locat.farenheir;
-// function that calls the position
-
-function getLocation () {
-  if (navigator.geolocation) {
-    navigator.geolocation.watchPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocationis not supported by the browser.";
-  }
-};
-getLocation();
-// function that returns the position
-theLocationIs="";
-function showPosition(position) {
-
-  locat.lat = (position.coords.latitude);
-  locat.long = (position.coords.longitude);
-
-
-
-
-
-
-console.log(
-
-theLocationIs = locat.urlStart + locat.lat + locat.longi + locat.long + locat.key + locat.celsius);
-
-return theLocationIs;
-
-callData();
-gotData();
-};
-
-
-
-function callData(){
-
-    //JSON.parse(theLocationIs, gotData);
-    JSON.parse("api.openweathermap.org/data/2.5/weather?lat=43.433051299999995&lon=6.7378997&appid=88ed677935f66ca8ba614806bb5b6618&units=metric", gotData);
-};
-
-function gotData (dataWeather){
-
-    console.log(dataWeather);
-    document.getElementById('city').innerHTML = dataWeather;
-};
-
-console.log('im here');
-// now we call the JSON API
-/*
-console.log(array);
-lat = array[0];
-long = array[1];
-console.log(lat);
-console.log(long);
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*var country_name = document.getElementById('country');
-var country_code = document.getElementById('country_code')
-var state = document.getElementById('state');
-var city = document.getElementById('city');
-var postal = document.getElementById('postal');
-var latitude = document.getElementById('latitude');
-var longitude = document.getElementById('longitude');
-var ip = document.getElementById('ipv4');
-
-
-function callback(data)
-{
-  country_code.innerHTML = data.country_code;
-  //country_name.innerHTML = data.country_name;
-  state.innerHTML = data.state;
-  city.innerHTML = data.city;
-  postal.innerHTML = data.postal;
-  latitude.innerHTML = data.latitude;
-  longitude.innerHTML = data.longitude;
-  //ip.innerHTML = data.IPv4;
-};
-
-
-
-
-
-
-
-
-var script = document.createElement('script');
-	script.type = 'text/javascript';
-	script.src = 'https://geoip-db.com/jsonp';
-	var h = document.getElementsByTagName('script')[0];
-	h.parentNode.insertBefore(script, h);
-
-
-
-
-
-
-
-
-    var lat = select('#latitude');
-    var lon = select('#longitude');
-    var key = "&appid=88ed677935f66ca8ba614806bb5b6618";
-    var urlStart = "api.openweathermap.org/data/2.5/weather?lat=";
-    var long = "&lon=";
-    var url = urlStart + latX + long + lonY + key ;
-    var fahrenheir = "&units=imperial";
-    var celsius = "&units=metric";
-
-    function callData(){
-      loadJSON(url, gotData);
-    };
-
-    function gotData (dataWeather){
-      console.log(dataWeather);
-    };
-
-
-
-
-
-
-*/
-
-
-
-
-
-
-
-
-//api.openweathermap.org/data/2.5/weather?q={city name}
-//api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}
-/*var key = "&appid=88ed677935f66ca8ba614806bb5b6618";
-var urlStart = "api.openweathermap.org/data/2.5/weather?lat={";
-var long = "}&lon={"; // after "}";
-var url = urlStart + latX + long + lonY + "}";
-
-*/
-
-
-
-
-
-
-
-/*
-  function callData(){
-    loadJSON(url, gotData);
-  };
-
-  function gotData (data){
-    console.log(data);
-  };*/
-/*
-getJSON(api.openweathermap.org/data/2.5/weather?lat={data.latitude}&lon={data.longitude});
-*/
-
-/*
-console.log("api.openweathermap.org/data/2.5/weather?lat={data.latitude}&lon={data.longitude}");
-*/
-
-
-
-
-
-
-
-
-
-
-/*var time = "";
-var myTime = "";
-var loc = document.getElementById("location");
-var myLoc = "";
-var temp = "";
-var myTemp = "";
-var logo = "";
-var myLogo = "";
-var fahrenheit = "";
-
-
-
-
-
-var country = document.getElementById('country');
-var state = document.getElementById('state');
-var city = document.getElementById('city');
-var postal = document.getElementById('postal');
-var latitude = document.getElementById('latitude');
-var longitude = document.getElementById('longitude');
-var ip = document.getElementById('ipv4');
-
-function callback(data)
-{
-  country.innerHTML = data.country_name;
-  state.innerHTML = data.state;
-  city.innerHTML = data.city;
-  postal.innerHTML = data.postal;
-  latitude.innerHTML = data.latitude;
-  longitude.innerHTML = data.longitude;
-  ip.innerHTML = data.IPv4;
-}
-*/
-/*
-
-
-var script = document.createElement('script');
-//script.type = 'text/javascript';
-script.src = 'https://geoip-db.com/jsonp';
-var h = document.getElementsByTagName('script')[0];
-//h.parentNode.insertBefore(script, h);
-
-
-
-
-// Location avec JSON
-
-var country = document.getElementById('country');
-var state = document.getElementById('state');
-var city = document.getElementById('city');
-var postal = document.getElementById('postal');
-var latitude = document.getElementById('latitude');
-var longitude = document.getElementById('longitude');
 var country_code = document.getElementById('country_code');
-var ip = document.getElementById('ipv4');
 
-function callback (data)
+var city = document.getElementById('city');
+var state = document.getElementById('state');
+
+var theData;
+function callback(data)
 {
-  country.innerHTML = data.country_name;
+  country_code.innerHTML = data.country_code;
   state.innerHTML = data.state;
   city.innerHTML = data.city;
-  postal.innerHTML = data.postal;
-  latitude.innerHTML = data.latitude;
-  longitude.innerHTML = data.longitude;
-  country_code.innerHTML = data.country_code;
-  ip.innerHTML = data.IPv4;
+
+console.log(data.country_name);
+  console.log(data.city);
+   console.log(data.state);
+
+  createURL(data.latitude.toString(), data.longitude.toString());
+ };
+
+ function createURL(x, y) {
+  var theURL = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/405ddb43858fde92eea9e80833bbcf06/" + x + "," + y + "?units=si";
+   console.log(theURL);
+
+   callData(theURL);
+ };
+
+
+
+
+function callData(address) {
+
+
+  var request = new XMLHttpRequest();
+request.open('GET', address, true);
+
+var theRequest = new XMLHttpRequest();
+
+theRequest.open('get', address, true);
+  theRequest.onload = function () {
+     theData = JSON.parse(theRequest.response);
+
+  weather(theData);
+
+  };
+  theRequest.send();
+
+
 };
 
-console.log(country);
+var timezone = document.getElementById('timeZone');
+var temperature = document.getElementById('temp');
+var temperatureMax = document.getElementById('maxTemp');
+var temperatureMin = document.getElementById('minTemp');
+//var humidity = document.getElementById('');
+//var windSpeed = document.getElementById('');
 
-*/
+//var icon = document.getElementById('');
 
-// LOCATION (code)
-/*
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    loc.innerHTML =  "Geolocation is not supported by this browser.";
-  }
-};
+function weather (data){
+  timezone.innerHTML = data.timezone;
+  //console.log(data.timezone);
+  temperature.innerHTML = data.currently.temperature;
+  //console.log(data.currently.temperature);
+  temperatureMax.innerHTML = data.daily.data[0].temperatureMax;
+  //console.log(data.daily.data[0].temperatureMax);
+  temperatureMin.innerHTML = data.daily.data[0].temperatureMin;
+  //console.log(data.daily.data[0].temperatureMin);
+  //humidity.innerHTML = data.currently.humidity;
+  //console.log(data.currently.humidity);
+  //windSpeed.innerHTML = data.currently.windSpeed;
+  //console.log(data.currently.windSpeed);
 
-function showPosition(position) {
-  loc.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
-};
-getLocation(); // get the location on load
-*/
-
-// LOCATION (town Country)
+// icon & background image
+console.log(data.currently.icon);
+}
 
 
-// TEMPERATUR
-/*function convertTo(celsius) {
-  fahrenheit = celsius * 9/5 + 32;
-  return fahrenheit;
-};
-*/
+ var script = document.createElement('script');
+ 	script.type = 'text/javascript';
+ 	script.src = 'https://geoip-db.com/jsonp';
+ 	var h = document.getElementsByTagName('script')[0];
+ 	h.parentNode.insertBefore(script, h);
