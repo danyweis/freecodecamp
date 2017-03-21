9fed0d7ccf0b0efa4ed73a1d010d1e0e9b34f8ea
Dany <danyweismail@gmail.com>
2017-03-21 21:33:25 +0100

    je fais de nouvel recherche comme je suis bloqué

--- projects/twitch/twitch.js
+++ projects/twitch/twitch.js
@@ -1,58 +1,33 @@
-var arrChannelElt = ["medrybw", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var arrChannelElt = ["medrybw", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"]; //, "OgamingSC2"
 var arrCree = [];
 var arrOn = [];
+var arrOff = [];
 
-/*function all() {
+function all() {
     document.getElementById("contenu").innerHTML = "";
-
+    arrCree = []
     for (var i = 0; i < arrChannelElt.length; i++) {
-         var channelName = arrChannelElt[i];
-         var request = new XMLHttpRequest();
-         request.open('GET', 'https://wind-bow.glitch.me/twitch-api/channels/' + channelName, true); //users streams
-         request.onload = function() {
-             if (this.status >= 200 && this.status < 400) {
-                 var data = JSON.parse(this.responseText);
-     var responseBoxElt = document.createElement("div");
-     responseBoxElt.style.height = "100px";
-     responseBoxElt.style.marginTop = "10px";
-     responseBoxElt.style.padding = "10px 0 0 10px";
-     responseBoxElt.style.border = "1px solid #381975";
-
-     var responseImgElt = document.createElement("img");
-     responseImgElt.setAttribute("src", data.logo);
-     responseImgElt.style.height = "75px";
-     responseImgElt.style.marginRight = "10px";
-     responseImgElt.style.borderRadius = "50%";
-     responseImgElt.style.float = "left";
-     var responseNameElt = document.createElement("h4");
-     var responseChannelElt = document.createElement("a");
-     responseChannelElt.textContent = data.display_name;
-     responseChannelElt.href = data.url;
-     responseChannelElt.style.textDecoration = "none";
-     responseChannelElt.style.color = "#fff";
-     responseChannelElt.setAttribute("target", "_blank");
-
-
-     responseNameElt.appendChild(responseChannelElt);
-     responseBoxElt.appendChild(responseImgElt);
-     responseBoxElt.appendChild(responseNameElt);
-
-     document.getElementById("contenu").appendChild(responseBoxElt);
-
-     arrCree.push(data);
-    } else {
-    // ERROR
-}
-};
+        var channelName = arrChannelElt[i];
+        var request = new XMLHttpRequest();
+        request.open('GET', 'https://wind-bow.glitch.me/twitch-api/users/' + channelName, true); //users streams
+        request.onload = function() {
+            if (this.status >= 200 && this.status < 400) {
+                var data = JSON.parse(this.responseText);
+                arrCree.push(data);
+            } else {
+                // ERROR
+            }
+        };
         request.send();
         channelName = "";
     }
 }
 
-all();*/
+all();
 
 function online() {
     document.getElementById("contenu").innerHTML = "";
+    arrOn = [];
     for (var j = 0; j < arrChannelElt.length; j++) {
         var streamName = arrChannelElt[j];
         var requestStream = new XMLHttpRequest();
@@ -60,41 +35,7 @@ function online() {
         requestStream.onload = function() {
             var test = JSON.parse(this.responseText);
             if (test.stream !== null) {
-
-
-                var responseTitleElt = document.createElement("p");
-                responseTitleElt.textContent = test.stream.channel.status;
-
-
-                var responseBoxElt = document.createElement("div");
-                responseBoxElt.style.height = "100px";
-                responseBoxElt.style.marginTop = "10px";
-                responseBoxElt.style.padding = "10px 0 0 10px";
-                responseBoxElt.style.border = "1px solid #381975";
-                responseBoxElt.style.backgroundColor = "#55AA55";
-
-                var responseImgElt = document.createElement("img");
-                responseImgElt.setAttribute("src", test.stream.channel.logo);
-                responseImgElt.style.height = "75px";
-                responseImgElt.style.marginRight = "10px";
-                responseImgElt.style.borderRadius = "50%";
-                responseImgElt.style.float = "left";
-                var responseNameElt = document.createElement("h4");
-                var responseChannelElt = document.createElement("a");
-                responseChannelElt.textContent = test.stream.channel.display_name;
-                responseChannelElt.href = test.stream.channel.url;
-                responseChannelElt.style.textDecoration = "none";
-                responseChannelElt.style.color = "#fff";
-                responseChannelElt.setAttribute("target", "_blank");
-
-
-                responseNameElt.appendChild(responseChannelElt);
-                responseBoxElt.appendChild(responseImgElt);
-                responseBoxElt.appendChild(responseNameElt);
-                responseBoxElt.appendChild(responseTitleElt);
-                document.getElementById("contenu").appendChild(responseBoxElt);
-
-
+                arrOn.push(test);
             } else {
                 //responseBoxElt.style.backgroundcolor = "#55AA55";
             }
@@ -104,7 +45,7 @@ function online() {
     }
 }
 
-
+online();
 
 document.getElementById("all").addEventListener("click", function() {
     all();
@@ -118,17 +59,24 @@ document.getElementById("online").addEventListener("click", function() {
 
 document.getElementById("offline").addEventListener("click", function() {
     document.getElementById("contenu").innerHTML = "";
-    for (var j = 0; j < arrChannelElt.length; j++) {
-        var streamName = arrChannelElt[j];
-        var requestStream = new XMLHttpRequest();
-        requestStream.open('GET', 'https://wind-bow.glitch.me/twitch-api/streams/' + streamName, true);
-        requestStream.onload = function() {
-            var test = JSON.parse(this.responseText);
-            arrOn.push(test);
+    arrOff = [];
+
+
+    for (i = 0; i < arrCree.length; i++) {
+
+
+        if (arrOn.indexOf(arrCree[i].display_name) === -1) {
+            arrOff.push(arrCree[i]);
         }
     }
-    console.log(arrCree);
+    for (j = 0; j < arrOn.length; j++) {
+        if (arrCree.indexOf(arrOn[j].stream.channel.display_name) === -1) {
+            arrOff.push(arrOn[j]);
+        }
+    }
+
     console.log(arrOn);
+    console.log(arrOff);
 });
 
 
@@ -137,20 +85,34 @@ document.getElementById("offline").addEventListener("click", function() {
 
 
 
-function test() {
-    for (var i = 0; i < arrChannelElt.length; i++) {
-        var channelName = arrChannelElt[i];
-        var request = new XMLHttpRequest();
-        request.open('GET', 'https://wind-bow.glitch.me/twitch-api/channels/' + channelName, true); //users streams
-        request.onload = function() {
-            if (this.status >= 200 && this.status < 400) {
-                var data = JSON.parse(this.responseText);
-                arrCree.push(data);
-            }
-            request.send();
-            channelName = "";
-        }
-    }
-}
-test();
-console.log(arrCree);
+
+
+/*
+var responseBoxElt = document.createElement("div");
+responseBoxElt.style.height = "100px";
+responseBoxElt.style.marginTop = "10px";
+responseBoxElt.style.padding = "10px 0 0 10px";
+responseBoxElt.style.border = "1px solid #381975";
+
+var responseImgElt = document.createElement("img");
+responseImgElt.setAttribute("src", data.logo);
+responseImgElt.style.height = "75px";
+responseImgElt.style.marginRight = "10px";
+responseImgElt.style.borderRadius = "50%";
+responseImgElt.style.float = "left";
+var responseNameElt = document.createElement("h4");
+var responseChannelElt = document.createElement("a");
+responseChannelElt.textContent = data.display_name;
+responseChannelElt.href = data.url;
+responseChannelElt.style.textDecoration = "none";
+responseChannelElt.style.color = "#fff";
+responseChannelElt.setAttribute("target", "_blank");
+
+
+responseNameElt.appendChild(responseChannelElt);
+responseBoxElt.appendChild(responseImgElt);
+responseBoxElt.appendChild(responseNameElt);
+
+document.getElementById("contenu").appendChild(responseBoxElt);
+
+*/
